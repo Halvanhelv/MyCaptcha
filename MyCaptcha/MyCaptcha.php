d7d44b298aa386e3412e23f1b7811f8fa3a6687d
IG <omniacinis@gmail.com>
2026-01-08 03:07:16 +0400

    Translate Russian comments to English

--- MyCaptcha/MyCaptcha.php
+++ MyCaptcha/MyCaptcha.php
@@ -6,26 +6,26 @@ class MyCaptcha extends Text
 
     public static function get()
     {
-        $line_num = rand(3, 7); // рандомное количество линий на капче
-        $code = self::generate_text(); // принимаем рандомные символы
-        $img_arr = ["1.jpeg", '2.jpg', '3.jpg']; //рандомный фон
-        $img = array_rand($img_arr, 1); // рандомный индекс из массива фонов
-        $img = ImageCreateFromJPEG(__DIR__ . '/background/' .$img_arr[$img]); //  присваем фон по индексу
-        $font =  __DIR__ . '/fonts/arial.ttf';  // путь к шрифту
-        $w = imagesx($img) / 2; //центр изображения по вертикали
-        $h = imagesy($img) / 2; // центр изображения по горизонтали
-        $font_box = imagettfbbox(rand(19, 30) , 0, $font, $code); // рамка текста
-        $x = $w - round(($font_box[2] - $font_box[0]) / 2);  //расположение текста на фоне
+        $line_num = rand(3, 7); // random number of lines on captcha
+        $code = self::generate_text(); // generate random characters
+        $img_arr = ["1.jpeg", '2.jpg', '3.jpg']; // random background
+        $img = array_rand($img_arr, 1); // random index from backgrounds array
+        $img = ImageCreateFromJPEG(__DIR__ . '/background/' .$img_arr[$img]); // assign background by index
+        $font =  __DIR__ . '/fonts/arial.ttf';  // path to font
+        $w = imagesx($img) / 2; // image center vertically
+        $h = imagesy($img) / 2; // image center horizontally
+        $font_box = imagettfbbox(rand(19, 30) , 0, $font, $code); // text bounding box
+        $x = $w - round(($font_box[2] - $font_box[0]) / 2);  // text position on background
         $y = $h - round(($font_box[7] - $font_box[1]) / 2);
 
-        for ($i = 0;$i < $line_num;$i++) // случаное количество линий на капче
+        for ($i = 0;$i < $line_num;$i++) // random number of lines on captcha
         {
-            $line_color = imagecolorallocate($img, rand(0, 150) , rand(0, 100) , rand(0, 150)); // рандомный цвет c изображения
+            $line_color = imagecolorallocate($img, rand(0, 150) , rand(0, 100) , rand(0, 150)); // random color
             imageline($img, rand(0, 20) , rand(1, 50) , rand(150, 180) , rand(1, 50) , $line_color);
 
         }
 
-        for ($i = 0;$i < strlen($code);$i++) //каждая буква рандомна наклонена
+        for ($i = 0;$i < strlen($code);$i++) // each letter is randomly tilted
         {
             $text_color = imagecolorallocate($img, rand(0, 150) , rand(0, 100) , rand(0, 150));
             $letter = substr($code, $i, 1);
